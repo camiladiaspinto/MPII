3fcc616d564736395a789ab5e5cfac0b07c376a1
camiladiaspinto <62036640+camiladiaspinto@users.noreply.github.com>
2021-03-19 21:44:30 +0000

    Create init.h

---
+++ init.h
@@ -0,0 +1,7 @@
+#include "struct.h"
+
+TAREFA criaTarefa();
+char *datai(TAREFA* tarefas, int d);
+void consultaTarefa(TAREFA* tarefas, int counter);
+void eliminaTarefa(TAREFA* tarefas, int counter);
+void alteraTarefa(TAREFA* tarefas, int counter);
